27bf003ec1b77763ef57de1cea702e85e8d127b6
Oxke <oseaetobia@gmail.com>
2023-08-25 17:51:04 +0200

    deleted useless things

--- objects/errors.py
+++
@@ -1,9 +0,0 @@
-class TotalExtinction(Exception):
-    def __init__(self):
-        super().__init__(self, "No animal left on the simulation")
-
-
-class AlreadyDeadError(Exception):
-    def __init(self, message):
-        super().__init__(self, message)
-
